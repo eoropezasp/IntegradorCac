4e1b87a1b6022239f43689bd9f690cb8f9c97ad8
emma <eoropezasp@gmail.com>
2023-05-20 22:15:16 -0300

    logica de cobro

--- js/ventaTicket.js
+++ js/ventaTicket.js
@@ -1,3 +1,7 @@
+var resultado=0;
+const PRECIO_TICKET=200;
+
+
 let nombre=document.getElementById('nombre'),
 apellido=document.getElementById('apellido'),
 email=document.getElementById('email'),
@@ -5,11 +9,17 @@ cantidad=document.getElementById('cant'),
 categoria=document.getElementById('categoriaSelect'),
 totalAPagar=document.getElementById('total_precio');
 
+
 function calcularCompra(){
 if(comprobarCampos()){
+resultado= (PRECIO_TICKET-(PRECIO_TICKET*parseInt(categoria.value) ))*parseInt(cantidad.value) ;
+console.log(resultado + typeof(resultado));
+
+totalAPagar.innerHTML=resultado;
 console.log("pasamos!!!");
 }else{
     console.log("no pasamos");
+    
 }
 }
 
